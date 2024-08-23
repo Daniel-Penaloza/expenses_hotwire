51e1deaf135733d7d9e0dd87152ca807af358abb
Daniel Penaloza <danielpenalozatj@gmail.com>
2024-08-22 19:51:54 -0700

    implement charts on index view

--- app/models/monthly_item.rb
+++ app/models/monthly_item.rb
@@ -1,4 +1,6 @@
 class MonthlyItem < ApplicationRecord
+  MONTHLY_LIMIT = 42_000
+
   has_many :items, dependent: :destroy
 
   enum month: {
@@ -18,7 +20,7 @@ class MonthlyItem < ApplicationRecord
 
 
   def msc_by_month
-    items.total_incomes_applied- items.total_outcomes_applied
+    items.total_incomes_applied - items.total_outcomes_applied
   end
 
   def month_incomes
@@ -33,7 +35,39 @@ class MonthlyItem < ApplicationRecord
     { incomes: month_incomes, outcomes: month_outcomes } if items.any?
   end
 
+  def self.budget_chart
+    current_month = MonthlyItem.find_by(month: Time.now.strftime('%B').downcase)
+
+    if (MONTHLY_LIMIT - current_month.month_outcomes).negative?
+      {  budget_overrun: (MONTHLY_LIMIT - current_month.month_outcomes).abs }
+    else
+      { monthly_limit: MONTHLY_LIMIT, outcomes: MONTHLY_LIMIT - current_month.month_outcomes }
+    end
+  end
+
   def self.accumulated
     MonthlyItem.all.inject(0) { |sum, element| sum + element.msc_by_month }
   end
+
+  # TODO: refactor this
+  def self.chart_info
+    data = [
+      ['Month', 'Incomes', 'Outcomes', 'Profit']
+    ]
+
+    self.all.each do |m|
+      data << [ m.month, m.month_incomes, m.month_outcomes, m.msc_by_month ]
+    end
+
+    chart_info = (1..3).map do |i|
+      {
+        name: data.first[i],
+        data: data[1..-1].map { |x| [ x[0], x[i] ] }
+      }
+    end
+
+    chart_info
+  end
+
+
 end
